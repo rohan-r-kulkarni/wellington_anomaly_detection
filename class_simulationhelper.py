73fd0a8352321bb4690e13f76370221a40442006
rl3279 <rl3279@columbia.edu>
2022-12-04 17:16:18 -0500

    Added documentation for class_simulationhelper.py

--- class_simulationhelper.py
+++ class_simulationhelper.py
@@ -4,40 +4,80 @@ from collections.abc import Iterable
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import cm
 from scipy.stats import norm
+from typing import Iterable
+
 
 class SimulationHelpers:
+    def __prepare_ax(self, i: int, n: int, row_lim: int, ax: plt.Axes):
+        """
+        Private function to aid graphing. Only called in self.plot().
 
-    def __prepare_ax(self, i, n, row_lim, ax):
-        if n == 1 :
+        :param i: index of ax
+        :type i: int
+        :param n: total number of subplots in the ax object
+        :type n: int
+        :param row_lim: the maximum number of subplots per row
+        :type row_lim: int
+        :param ax: the matplotlib.Axes object
+        :type ax: plt.Axes
+        :rtype: plt.Axes
+        """
+        if n == 1:
             return ax
         if n > row_lim:
-            return ax[i//row_lim,i%row_lim]
+            return ax[i // row_lim, i % row_lim]
         else:
-            return ax[i%row_lim]
+            return ax[i % row_lim]
 
     def zip_series_for_plot(self, *args):
         """
         zipping time series that need to be plotted together.
+
+        :param args: time series Iterable
+        :type args: Iterable, optional
         """
         return [list(a) for a in zip(*args)]
 
-    def plot(self, *args, outliers = None, figsize=(20, 14), func=None, row_lim = 4):
+    def plot(
+        self,
+        *args,
+        outliers: Iterable = None,
+        figsize: Iterable = (20, 14),
+        func: str = None,
+        row_lim: int = 4
+    ):
         """
-        Plot time series and mark out outliers. 
+        Plot time series and mark out outliers.
 
-        Format of arg in args:
-            arg = {
-                "data": [series_1, series_2, ...]
-                "outliers": [outlier_idx_1, outlier_idx_2, ...]
-            }
+        Format:
+            plot(
+                series_1, series_2,
+                ...,
+                outliers=[outlier_idx_1, outlier_idx_2, ...]
+            )
+
+        :param args: time series Iterable, optional
+        :type args: Iterables, e.g. `pd.Series`.
+        :outliers: list of outlier indices to be overlayed on top of each subplot.
+            Must be of the same length as args. Default to `None`.
+        :type outliers: List[Iterable[int]], optional
+        :param figsize: Iterable specifying plt figsize, default to (20, 14)
+        :type figsize: Iterable, conventionally tuple, optional.
+        :param func: transformation applied to each series before plotting.
+            only ['log', 'ret', 'diff'] are implemented.
+            - 'log': equivalent to lambda x: np.log(x)
+            - 'ret': equivalent to lambda x: x.pct_change() in pandas
+            - 'diff': equivalent to lambda x: x.diff() in pandas
+            optional, default to `None`.
+        :type func: str
+        :param row_lim: the maximum number of subplots per row
+        :type row_lim: int
         """
         n = len(args)
         if n > row_lim:
-            # _, ax = plt.subplots(n//row_lim + 1, min(row_lim, n), sharey=True)
-            _, ax = plt.subplots(n//row_lim + 1, row_lim, sharey=True)
+            _, ax = plt.subplots((n - 1) // row_lim + 1, row_lim, sharey=True)
         else:
-            _, ax = plt.subplots(1, min(row_lim, n), sharey=True) 
-        # fig, ax = plt.subplots(n//row_lim + 1, min(row_lim, n), sharey=True)
+            _, ax = plt.subplots(1, min(row_lim, n), sharey=True)
 
         for i, arg in enumerate(args):
             if isinstance(arg, list):
@@ -47,90 +87,148 @@ class SimulationHelpers:
 
             if func == "log":
                 for a in arg:
-                    np.log(a).plot(grid=True, figsize=figsize, 
-                        # ax=ax[i//row_lim,i%row_lim] if n > row_lim else ax[i%row_lim]
-                        ax = self.__prepare_ax(i, n, row_lim, ax)
+                    np.log(a).plot(
+                        grid=True,
+                        figsize=figsize,
+                        ax=self.__prepare_ax(i, n, row_lim, ax),
                     )
             elif func == "ret":
                 for a in arg:
-                    a.pct_change().plot(grid=True, figsize=figsize, 
-                        # ax=ax[i//row_lim,i%row_lim] if n > row_lim else ax[i%row_lim]
-                        ax = self.__prepare_ax(i, n, row_lim, ax)
+                    a.pct_change().plot(
+                        grid=True,
+                        figsize=figsize,
+                        ax=self.__prepare_ax(i, n, row_lim, ax),
                     )
             elif func == "diff":
                 for a in arg:
-                    a.diff().plot(grid=True, figsize=figsize, 
-                        # ax=ax[i//row_lim,i%row_lim] if n > row_lim else ax[i%row_lim]
-                        ax = self.__prepare_ax(i, n, row_lim, ax)
+                    a.diff().plot(
+                        grid=True,
+                        figsize=figsize,
+                        ax=self.__prepare_ax(i, n, row_lim, ax),
                     )
             else:
                 for a in arg:
-                    a.plot(grid=True, figsize=figsize, 
-                        # ax=ax[i//row_lim,i%row_lim] if n > row_lim else ax[i%row_lim]
-                        # ax=ax[i//row_lim,i%row_lim] if n > 1 else ax
-                        ax = self.__prepare_ax(i, n, row_lim, ax)
+                    a.plot(
+                        grid=True,
+                        figsize=figsize,
+                        ax=self.__prepare_ax(i, n, row_lim, ax),
                     )
-            
+
         if outliers is not None:
             if not isinstance(outliers, Iterable) or len(outliers) != len(args):
-                raise RuntimeError("Invalid outlier passed. Must have the same length as *args.")
-        
-            for i, l in enumerate(outliers):
-                cmap= iter(cm.rainbow(np.linspace(0, 1, len(l))))
-                for point in l:
-                    pos = ax[i//row_lim,i%row_lim] if n > row_lim else ax[i%row_lim]
-                    pos.axvline(x = point, color = next(cmap), linestyle = "--", alpha = 0.8)
+                raise RuntimeError(
+                    "Invalid outlier passed. Must have the same length as *args."
+                )
 
-    def gen_rand_cov_mat(self, n: int, random_seed=None, sigma=None):
+            for i, l in enumerate(outliers):
+                cmap = iter(cm.rainbow(np.linspace(0, 1, len(l))))
+                for point in l:
+                    pos = (
+                        ax[i // row_lim, i % row_lim]
+                        if n > row_lim
+                        else ax[i % row_lim]
+                    )
+                    pos.axvline(x=point, color=next(cmap), linestyle="--", alpha=0.8)
+
+    def gen_rand_cov_mat(self, n: int, random_seed: int = None):
+        """Generates a positive definite random covariance matrix.
+
+        :param n: number of rows/columns of the matrix
+        :type n: int
+        :param random_seed: random seed, default to `None`.
+        :type random_seed: int
+        :rtype: np.array
+        """
         if random_seed is not None:
             np.random.seed(random_seed)
-            
-        A = np.random.rand(n,n)
+
+        A = np.random.rand(n, n)
         Cov = A.dot(A.T)
-        if sigma is not None:
-            if isinstance(sigma, float):
-                Cov[np.diag_indices_from(Cov)] = sigma**2
-            elif len(sigma) == n:
-                Cov[np.diag_indices_from(Cov)] = np.array(sigma)**2
         return Cov
 
-    def corr_from_cov(self, x):
+    def corr_from_cov(self, x: np.array):
+        """Convert a covariance matrix to a correlation matrix.
+
+        :param x: Covariance matrix
+        :type x: np.array
+        :rtype: np.array
+        """
         v = np.sqrt(np.diag(x))
         outer_v = np.outer(v, v)
         corr = x / outer_v
         corr[x == 0] = 0
         return corr
 
-    def brownian_process(self, n: int, mu=0.1, sigma=0.01, S0=1):
+    def brownian_process(self, n: int, mu=0.1, sigma: float = 0.01, S0: float = 1):
+        """Generate a brownian motion process.
+
+        :param n: length of series
+        :type n: int
+        :param mu: drift parameter
+        :type mu: float
+        :param sigma: volatility parameter
+        :type sigma: float
+        :param S0: initial position
+        :type S0: float
+        """
         dt = 1 / n
         t = np.linspace(0, 1, n)
         W = np.random.standard_normal(size=n)
         W = np.cumsum(W) * np.sqrt(dt)  ### standard brownian motion ###
         X = (mu - 0.5 * sigma**2) * t + sigma * W
-        return X*S0
-        
-    def standard_scaler(self, process):
+        return X * S0
+
+    def standard_scaler(self, process: np.array):
+        """Scales a series by its mean and std.
+
+        :param process: the time series to be scaled
+        :type process: np.array
+        :rtype: np.array
+        """
         mean = np.mean(process)
         std = np.std(process)
         return (process - mean) / std
 
     def gen_seasonality(
         self,
-        n,
-        amp,
-        freq,
-        contamination,
-        how_diffusion=None,
-        diffusion=0.1,
+        n: int,
+        amp: float,
+        freq: float,
+        contamination: float,
+        how_diffusion: str = "no",
+        diffusion: float = 0.1,
     ):
+        """Seasonality generation function copied from class `BaseSimulation`.
+
+        :param n: length of series
+        :type n: int
+        :param amp: amplitude parameter. Higher amp induces higher oscillation.
+        :type amp: float
+        :param freq: frequency parameter. Higher freq induces faster oscillation.
+        :type freq: float
+        :param contamination: the volatility of the random contamination introdced
+            to the seaonality.
+        :type contamination: float
+        :param how_diffusion: flag to specify how does the amplitude change over time.
+            only ['linear', 'sqrt', 'no'] are implemented.
+            - 'linear': amplitude grows linearly over time.
+            - 'sqrt': amplitude grows proportionally to the square root of time.
+            - 'no': no diffusion, constant. 
+            Default to be 'no'.
+        :type how_diffusion: str, optional.
+        :param diffusion: the diffusion multiplier. Higher diffusion implies faster diffusion.
+            Default to be 0.1.
+        :type diffusion: float, optional.
+        :rtype: np.array
+        """
 
         x = np.arange(0, n, 1)
-        w = self.brownian_process(n, mu=0, sigma = contamination)
+        w = self.brownian_process(n, mu=0, sigma=contamination)
         if how_diffusion == "linear":
             amp = x * amp * diffusion
         elif how_diffusion == "sqrt":
             amp = np.sqrt(x) * amp * diffusion
         elif how_diffusion == "no":
             pass
-        
-        return amp * np.cos(x*freq) + w
+
+        return amp * np.cos(x * freq) + w
