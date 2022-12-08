bae75b81d0da3d4cbe2848c2be0c9b6afa98de1e
rl3279 <rl3279@columbia.edu>
2022-12-08 12:42:13 -0500

    Added documentation for data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -7,15 +7,29 @@ sim = MultiSimulation()
 helper = SimulationHelpers()
 
 
-def gen_data(random_seed) -> pd.DataFrame:
+def gen_data(
+    random_seed: int, n: int = None, dim: int = None, n_outlier: int = None
+) -> pd.DataFrame:
+    """Use only a random seed to generate multivariate time series.
+
+    :param random_seed: random seed.
+    :type random_seed: int
+    :param n: number of periods of the time series, default to be `None`.
+    :type n: int, optional
+    :param dim: number of dimensions of the time series, default to be `None`
+    :type dim: int, optional
+    :rtype: pd.DataFrame
+    """
     # hyperparameters to randomize:
     # data length, covariance matrix, number of dimensions, number of outliers.
-
-    n = (np.random.choice(50) + 1) * 100
-    dim = np.random.choice(np.arange(5, 16, 1))
+    n = (np.random.choice(50) + 1) * 100 if n is None else n
+    dim = np.random.choice(np.arange(5, 16, 1)) if dim is None else dim
+    # manual specification of covairance matrix is not implemented.
+    # since speicifcation defeats the purpose of randomization
     cov_mat = helper.gen_rand_cov_mat(dim, random_seed=random_seed)
-    n_outlier = np.random.choice(15) + 1
+    n_outlier = np.random.choice(15) + 1 if n_outlier is None else n_outlier
 
+    # generate correlated brownian proceses with correlated outliers.
     data = sim.correlated_brownian_processes_with_CO(
         n=n,
         cov_mat=cov_mat,
@@ -27,9 +41,11 @@ def gen_data(random_seed) -> pd.DataFrame:
         mu=0,
     )
 
+    # scale and dropna
     data = np.array([d[~np.isnan(d)] for d in data])
     data = np.array([helper.standard_scaler(d) for d in data])
 
+    # add seasonalilties
     perturbed_data = np.array(
         [
             sim.add_seasonality(
@@ -41,12 +57,15 @@ def gen_data(random_seed) -> pd.DataFrame:
         ]
     )
 
+    # add random shifts
     perturbed_data = np.array(
         [
             sim.add_shift(d, how="full_random", random_seed=random_seed)
             for d in perturbed_data
         ]
     )
+
+    # for future developers: can add other random transformations should one desire
 
     df = pd.DataFrame(perturbed_data.T)
     # dropna
