fc2d206f7c4b3c0a4531c590644ce9221ac6d680
rl3279 <rl3279@columbia.edu>
2022-12-09 18:15:14 -0500

    Added metric; added documentation to lstm_autoencoder and model_exec

--- lstm_autoencoder.py
+++ lstm_autoencoder.py
@@ -1,138 +1,218 @@
+from collections.abc import Iterable
+import random
 from class_basesimulation import BaseSimulation
+from class_simulationhelper import SimulationHelpers
+from pyod.utils.stat_models import pairwise_distances_no_broadcast
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 import keras
+from typing import List
 
 
 class DataGeneration:
-    def __init__(self, total_time = 10000, seq_size = 100):
+    """Class created to fit data simulated by BaseSimulation to preliminary models
+    :param total_time: number of observations to generate
+    :type total_time: int
+    :param seq_size: the size of the look-back window
+    :type seq_size: int
+    :param n_feature: number of features (dimensions)
+    :type n_feature: int
+    """
 
-        self.total_time = total_time
-        self.seq_size = seq_size
-
-        sim = BaseSimulation()
-        self.data = sim.geom_brownian_process(n=total_time, mu=0.1, sigma=0.1)
-
+    def __init__(self, total_time: int = 10000, seq_size: int = 10, n_feature: int = 3):
+        self.total_time = total_time  # length of simulation duration
+        self.seq_size = seq_size  # length of looking back
+        self.n_feature = n_feature
+        self.sim = BaseSimulation()
 
     def to_sequences(self):
-        sequences = []
-        for i in range(len(self.data)-self.seq_size):
-            sequences.append(self.data[i:i+self.seq_size])
+        seq_normal = []
+        seq_outlier = []
+        for i in range(len(self.data_normal) - self.seq_size):
+            seq_normal.append(self.data_normal[i : i + self.seq_size])
+            seq_outlier.append(self.data_outlier[i : i + self.seq_size])
+        return np.array(seq_normal), np.array(seq_outlier)
 
-        return np.array(sequences)
+    def multi_data(self):
+        helper = SimulationHelpers()
+        sigma = 0.02
+        Sig = helper.gen_rand_cov_mat(
+            self.n_feature,
+            # sigma = sigma
+        )
 
-# class DataGeneration:
-#     def __init__(self, size=300, time_steps=100):
-#         self.size = size
-#         self.data_normal = []
-#         self.data_outlier = []
-#
-#         sim = BaseSimulation()
-#         for i in range(size):
-#             normal = sim.geom_brownian_process(n=time_steps, mu=0.1, sigma=0.1)
-#             normal_outlier = pd.Series(sim.add_outlier(normal, count=2, thresh_z=3))
-#
-#             self.data_normal.append(normal)
-#             self.data_outlier.append(normal_outlier)
-#
-#         self.data_normal = np.array(self.data_normal)
-#         self.data_outlier = np.array(self.data_outlier)
+        data = self.sim.correlated_brownian_process(
+            n=self.total_time, mu=0, cov_mat=Sig, S0=100
+        ).T
+
+        X = temporalize(X=data, seq_size=self.seq_size)
+
+        X = np.array(X)
+        X = X.reshape(X.shape[0], self.seq_size, self.n_feature)
+
+        return X
 
 
-class MyModel(tf.keras.Model):
-    def __init__(self, seq_size):
+class LSTM_Model_Base(tf.keras.Model):
+    """LSTM AutoEncoder base class, which allows the specification of LSTM-AE Architecture
+    via a list containing number of neurons per layers. e.g. [4,2,2,4] would specify
+    a 2-layer LSTM-AE model with 4 neurons in the first layer and 2 neurons in the second
+    layer of the encoder.
+
+    :param seq_size: size of look-back window
+    :type seq_size: int
+    :param n_feature: number of features
+    :type n_feature: int
+    :param layers: the LSTM-AE Architecture. Must be symmetric, otherwise a runtime error
+        is raised.
+    :type layers: List[int]
+    :param activation: activation function for each LSTM layer. Default to be tf.nn.tanh.
+        Note that during our experimentations, tf.nn.tanh is the only working option.
+    :type activation: Callable
+    """
+
+    def __init__(
+        self,
+        seq_size: int,
+        n_feature: int,
+        layers: List[int],
+        activation=tf.nn.tanh,
+        mid_activation=tf.nn.tanh,
+    ):
         super().__init__()
         self.seq_size = seq_size
-        self.lstm1 = tf.keras.layers.LSTM(128, activation = tf.nn.tanh, return_sequences=True)
-        self.lstm2 = tf.keras.layers.LSTM(64, activation = tf.nn.tanh, return_sequences = False)
-        self.repeat_v = tf.keras.layers.RepeatVector(self.seq_size)
-        self.lstm3 = tf.keras.layers.LSTM(64, activation=tf.nn.tanh, return_sequences=True)
-        self.lstm4 = tf.keras.layers.LSTM(128, activation=tf.nn.tanh, return_sequences=True)
-        self.time_distribute = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1))
+        self.n_feature = n_feature
+        self.Layers = []
+
+        # Check if layers present valid AutoEncoder architecture.
+        def is_ae_valid(l: List[int]):
+            """Checks if passed architecture is valid
+
+            :param l: LSTM-AE architecture
+            :type l: List[int]
+            :rtype: bool
+            """
+            if len(l) % 2 != 0:
+                return False
+            mid = len(l) // 2
+            for i in range(mid):
+                if l[i] != l[-(i + 1)]:
+                    return False
+            return True
+
+        # raise error if architecture passed in layers is not valid
+        if not is_ae_valid(layers):
+            raise RuntimeError("Not valid LSTM-AE architecture.")
+
+        # if valid architecture, add layers to self.Layers
+        for i in range(len(layers)):
+            # if not at the end of adding encoder layers, just add layer
+            # with return_sequences=True
+            if i != len(layers) // 2 - 1:
+                self.Layers.append(
+                    tf.keras.layers.LSTM(
+                        layers[i], activation=activation, return_sequences=True
+                    )
+                )
+            # if adding the last layer before repeat layer, set return_sequence
+            # to be False
+            else:
+                self.Layers.append(
+                    tf.keras.layers.LSTM(
+                        layers[i], activation=mid_activation, return_sequences=False
+                    )
+                )
+                self.Layers.append(tf.keras.layers.RepeatVector(self.seq_size))
+
+        self.Layers.append(
+            tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(n_feature))
+        )
 
     def call(self, inputs):
-        x = self.lstm1(inputs)
-        x = self.lstm2(x)
-        x = self.repeat_v(x)
-        x = self.lstm3(x)
-        x = self.lstm4(x)
-        x = self.time_distribute(x)
+        """Standard tensorflow class call method"""
+        x = inputs
+        for layer in self.Layers:
+            x = layer(x)
         return x
 
 
+def temporalize(X, seq_size: int):
+    """Prepare input data for LSTM layers by slicing the data into sequences.
+
+    :param seq_size: size of the look-back window
+    :type seq_size: int
+    :rtype: list
+    """
+    output_X = []
+
+    for i in range(len(X) - seq_size + 1):
+        output_X.append(X[i : i + seq_size, :])
+
+    return output_X
+
+
+def reconstruction(seq_data, n_features):
+    """Reconstruct the time series from sliced sequences.
+
+    :param seq_data: temporalized data
+    :type seq_data: np.ndarray
+    :param n_features: number of features
+    :type n_features: int
+    :rtype: np.ndarray
+    """
+    multi = []
+    for i in range(n_features):
+        uni_seq = seq_data[:, :, i]
+
+        uni = np.array([])
+        j = 0
+
+        for j in range(len(uni_seq)):
+            uni = np.append(uni, uni_seq[j, 0])
+
+        uni = np.append(uni, uni_seq[-1, 1:])
+        multi.append(uni)
+
+    multi = np.array(multi)
+    return multi.T
+
+
 if __name__ == "__main__":
-    #print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    d = DataGeneration()
-    data = d.data
+    # system setup
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+    print("Num GPUs Available: ", tf.config.list_physical_devices("GPU"))
+    random.seed(10)
+    helper = SimulationHelpers()
 
-    sequences = d.to_sequences()
-    x = sequences.reshape(sequences.shape + (1,))
+    # parameters
+    total_time = 20000
+    seq_size = 50
+    n_feature = 3
 
-    print(data.shape)
+    # data
+    d = DataGeneration(total_time=total_time, seq_size=seq_size)
+    x_normal = d.multi_data()
 
-    # # data_normal = d.data_normal
-    # # data_normal = data_normal.reshape(len(data_normal), len(data_normal[0]), 1)
-    # #
-    # # data_outlier = d.data_outlier
-    # # print(data_outlier)
-    # # data_outlier = data_outlier.reshape(len(data_outlier), len(data_outlier[0]), 1)
-    # #
-    # #
-    with tf.device("/CPU:0"):
-        model = MyModel(len(x[1]))
-        model.compile(optimizer='adam', loss='mse')
+    # model training and prediction
+    # model = LSTM_Model(seq_size, n_feature)
+    # model.compile(optimizer='adam', loss='mse')
+    # model.fit(x_normal, x_normal, epochs=80, batch_size=512)
+    # model.save('tmp_model')
 
-        model.fit(x, x, epochs=40, batch_size = 32)
-        prediction = model.predict(x)
-        print(prediction.shape)
+    # model prediction/reconstruction
+    model = keras.models.load_model("tmp_model")
+    pred = model.predict(x_normal)
 
+    x_reconstructed = reconstruction(x_normal, n_feature, seq_size)
+    pred_reconstructed = reconstruction(pred, n_feature, seq_size)
 
-        mae = np.mean(np.abs(prediction - x), axis = 1)
+    distances = pairwise_distances_no_broadcast(x_reconstructed, pred_reconstructed)
+    ind = np.argpartition(distances, -1000)[-1000:]
 
-
-
-        #reconstruct
-
-        normal = np.array([])
-        pred = np.array([])
-        i = 0
-        while i <=len(x)-100:
-            normal = np.append(normal, x[i].reshape([100,]))
-            pred = np.append(pred, prediction[i].reshape([100,]))
-            i = i + 100
-
-        plt.plot(range(len(x)), normal)
-        plt.plot(range(len(pred)), pred)
-        plt.show()
-        model.save('tmp_model')
-        # # model = keras.models.load_model('tmp_model')
-        # # print(model.summary())
-        # print("done")
-        #
-        # prediction = model.predict(x)
-        # print(prediction[0])
-        # #
-        #
-        # continuous = np.array([])
-        #
-        # i = 0
-        # while i <= len(prediction)-10:
-        #     print(prediction[i])
-        #     continuous = np.append(continuous, prediction[i])
-        #     i = i+10
-        #
-        # print(len(prediction))
-        # plt.plot(range(len(continuous)), continuous)
-        # plt.plot(range(10), x[0]-0.1)
-        # plt.plot(range(10,20), prediction[10])
-        # plt.plot(range(1000), data)
-        #
-        # #plt.plot(range(10:20), x[1])
-        # # plt.plot(range(10), prediction[0])
-        # plt.show()
-        # #
-        # # #print(model.call(data_normal))
-
-
+    print(ind)
+    print(distances[ind])
+    # plotting
+    helper.plot(args=x_reconstructed.T, preds=pred_reconstructed.T, markers=ind)
+    print("done")
--- model_exec.py
+++ model_exec.py
@@ -1,91 +1,183 @@
 import tensorflow as tf
 import keras
 import numpy as np
+from scipy.stats import norm
 from data_generation import gen_data
 from pyod.utils import pairwise_distances_no_broadcast
 from dense_autoencoder import DENSE_Model
-from lstm_autoencoder import LSTM_Model
 from lstm_autoencoder import reconstruction
 
-def quantile_outlier(l, thresh = 0.05):
-    cutoff = np.quantile([np.abs(x) for x in l], 1-thresh)
-    return [idx for idx in range(len(l)) if np.abs(l[idx]) >= cutoff]
 
-def bb_outlier(l, std_mult = 2.576):
-    ub = np.mean(l) + np.std(l) * std_mult
-    lb = np.mean(l) - np.std(l) * std_mult
-    return [idx for idx in range(len(l)) if l[idx] >= ub or l[idx] <= lb]
+class OutlierMetric:
+    """Class to organize outlier classification metric functions"""
+    def quantile_outlier(self, l, thresh=0.05):
+        """Decides that the top (100*thresh)% are outliers
+
+        :param l: data
+        :type l: Iterable
+        :param thresh: top quantile cutoff
+        :type thresh: float
+        :rtype: List[int]
+        """
+        cutoff = np.quantile([np.abs(x) for x in l], 1 - thresh)
+        return [idx for idx in range(len(l)) if np.abs(l[idx]) >= cutoff]
+
+    def bb_outlier(self, l, thresh=0.05):
+        """Decides that the observation k standard deviation outliers of mean
+        are outliers, like a bollinger band.
+
+        :param l: data
+        :type l: Iterable
+        :param thresh: used to determine std multiplier i.e. the observations with 
+            probability <= 0.05 are decided as outliers.
+        :type thresh: float
+        :rtype: List[int]
+        """
+        std_mult = norm.ppf(1-thresh/2)
+        ub = np.mean(l) + np.std(l) * std_mult
+        lb = np.mean(l) - np.std(l) * std_mult
+        return [idx for idx in range(len(l)) if l[idx] >= ub or l[idx] <= lb]
+
+    def iqr_outlier(self, l):
+        """Decides that the observations 1.5 IQR away from 75 quantile and 25
+        quantile are outliers.
+
+        :param l: data
+        :type l: Iterable
+        :rtype: List[int]
+        """
+        q75, q25 = np.percentile(l, [75, 25])
+        iqr = q75 - q25
+        return [
+            idx
+            for idx in range(len(l))
+            if l[idx] >= q75 + 1.5 * iqr or l[idx] <= q25 - 1.5 * iqr
+        ]
 
 
-def lstm_run(train_data, test_data, n_feature, batch_size, seq_size = 50, metric = "top", thresh=0.05, epoch = 80, early_stopping = False):
+def lstm_run(
+    model,
+    train_data,
+    test_data,
+    batch_size,
+    metric="top",
+    thresh=0.05,
+    epoch=80,
+    early_stopping=False,
+):
+    """Execute lstm model.
+
+    :param train_data: the training data
+    :type train_data: np.ndarray
+    :param test_data: the testing data
+    :type test_data: np.ndarray
+    :param batch_size: batch size
+    :type batch_size: int
+    :param metric: the metric to determine outlier
+    :type metric: str
+    :param thresh: probabiliy threshold for quantile outlier
+    :type thresh: float
+    :param epoch: number of epochs
+    :type epoch: int
+    :param early_stopping: if to employ early stop callback
+    :type early_stopping: bool
+    :rtype: (np.ndarray, Iterable)
+    """
+    metrics = OutlierMetric()
     # model training and prediction
-    model = LSTM_Model(seq_size, n_feature)
-    model.compile(optimizer='adam', loss='mse')
+    n_feature = model.n_feature
+    seq_size = model.seq_size
+    model.compile(optimizer="adam", loss="mse")
     if early_stopping:
-        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=10)
-        model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size, callbacks=[callback])
+        callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=10)
+        model.fit(
+            train_data,
+            train_data,
+            epochs=epoch,
+            batch_size=batch_size,
+            callbacks=[callback],
+        )
     else:
         model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size)
-    model.save('lstm_model')
+    model.save("lstm_model")
 
     # model prediction/reconstruction
-    model = keras.models.load_model('lstm_model')
+    model = keras.models.load_model("lstm_model")
     pred = model.predict(test_data)
 
-    test_reconstructed = reconstruction(test_data, n_feature, seq_size)
-    pred_reconstructed = reconstruction(pred, n_feature, seq_size)
+    test_reconstructed = reconstruction(test_data, n_feature)
+    pred_reconstructed = reconstruction(pred, n_feature)
 
     distances = pairwise_distances_no_broadcast(test_reconstructed, pred_reconstructed)
 
-    if metric == "top":
-        # top 1000 outliers
-        n = int(len(test_reconstructed) * thresh)
-        ind = np.argpartition(distances, -n)[-n:]
-    elif metric == "quantile":
-        ind = quantile_outlier(distances, thresh=thresh)
-    elif metric == "bb":
-        ind = bb_outlier(distances)
+    indx = {}
+    n = int(len(test_reconstructed) * thresh)
+    indx["top"] = np.argpartition(distances, -n)[-n:]
+    indx["quantile"] = metrics.quantile_outlier(distances, thresh=thresh)
+    indx["bb"] = metrics.bb_outlier(distances, thresh=thresh)
+    indx["iqr"] = metrics.iqr_outlier(distances)
+
+    if metric == "all":
+        ind = indx
+    else:
+        ind = indx[metric]
 
     return pred_reconstructed, ind
 
 
-def dense_run(train_data, test_data, n_feature, batch_size, metric = "top", thresh=0.05, epoch = 80):
-    # model training and prediction
-    model = DENSE_Model(n_feature)
-    model.compile(optimizer='adam', loss='mse')
-    model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size)
-    model.save('dense_model')
+# def dense_run(
+#     train_data, test_data, n_feature, batch_size, metric="top", thresh=0.05, epoch=80
+# ):
+#     # model training and prediction
+#     model = DENSE_Model(n_feature)
+#     model.compile(optimizer="adam", loss="mse")
+#     model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size)
+#     model.save("dense_model")
 
-    # model prediction/reconstruction
-    model = keras.models.load_model('dense_model')
-    pred = model.predict(test_data)
-    print(pred.shape, test_data.shape)
+#     # model prediction/reconstruction
+#     model = keras.models.load_model("dense_model")
+#     pred = model.predict(test_data)
+#     print(pred.shape, test_data.shape)
 
-    distances = pairwise_distances_no_broadcast(test_data, pred)
+#     distances = pairwise_distances_no_broadcast(test_data, pred)
 
-    if metric == "top":
-        # top 1000 outliers
-        n = int(len(test_data) * thresh)
-        ind = np.argpartition(distances, -n)[-n:]
-    elif metric == "quantile":
-        ind = quantile_outlier(distances, thresh=thresh)
-    elif metric == "bb":
-        ind = bb_outlier(distances)
+#     if metric == "top":
+#         # top 1000 outliers
+#         n = int(len(test_data) * thresh)
+#         ind = np.argpartition(distances, -n)[-n:]
+#     elif metric == "quantile":
+#         ind = quantile_outlier(distances, thresh=thresh)
+#     elif metric == "bb":
+#         ind = bb_outlier(distances)
 
-    return pred, ind
+#     return pred, ind
 
 
 def temporalize(X, seq_size):
+    """Prepare input data for LSTM layers by slicing the data into sequences.
+
+    :param seq_size: size of the look-back window
+    :type seq_size: int
+    :rtype: list
+    """
     # break data into seq_size
     output_X = []
 
     for i in range(len(X) - seq_size + 1):
-        output_X.append(X[i:i + seq_size, :])
+        output_X.append(X[i : i + seq_size, :])
 
     return np.array(output_X)
 
 
-def reconstruction(seq_data, n_features, seq_size):
+def reconstruction(seq_data, n_features):
+    """Reconstruct the time series from sliced sequences.
+
+    :param seq_data: temporalized data
+    :type seq_data: np.ndarray
+    :param n_features: number of features
+    :type n_features: int
+    :rtype: np.ndarray
+    """
     multi = []
     for i in range(n_features):
         uni_seq = seq_data[:, :, i]
@@ -105,8 +197,8 @@ def reconstruction(seq_data, n_features, seq_size):
 
 if __name__ == "__main__":
     # system setup
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-    print("Num GPUs Available: ", tf.config.list_physical_devices('GPU'))
+    print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+    print("Num GPUs Available: ", tf.config.list_physical_devices("GPU"))
 
     total_time = 30000
     seq_size = 50
@@ -116,20 +208,18 @@ if __name__ == "__main__":
 
     partition_size = int(len(data) * 3 / 4)
 
-    data_train = data[0: partition_size]
+    data_train = data[0:partition_size]
     data_test = data[partition_size:]
 
     data_train_seq = temporalize(data_train, seq_size)
     data_test_seq = temporalize(data_test, seq_size)
 
     lstm_pred, lstm_outliers = lstm_run(data_train_seq, data_test_seq)
-    dense_pred, dense_outliers = dense_run(data_train, data_test)
+    # dense_pred, dense_outliers = dense_run(data_train, data_test)
 
     print(f"Data test shape {data_test.shape}")
     print(f"lstm_pred shape {lstm_pred.shape}")
-    print(f"dense_pred shape {dense_pred.shape}")
+    # print(f"dense_pred shape {dense_pred.shape}")
 
     # plot the curves
     # TODO
-
-
