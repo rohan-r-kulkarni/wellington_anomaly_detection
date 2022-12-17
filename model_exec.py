9932c619ab8a2aab7b5b8de6970b52f11af78e6c
rl3279 <rl3279@columbia.edu>
2022-12-17 17:29:09 -0500

    Added history to lstm_run; added documentation to get_outliers

--- model_exec.py
+++ model_exec.py
@@ -1,11 +1,13 @@
 import numpy as np
 import keras
 import tensorflow as tf
+import warnings
 from data_generation import gen_data
 from lstm_autoencoder import reconstruction
 from pyod.utils import pairwise_distances_no_broadcast
 from scipy.stats import norm
 from sklearn.metrics import pairwise_distances
+
 
 class OutlierMetric:
     """Class to organize outlier classification metric functions"""
@@ -28,12 +30,12 @@ class OutlierMetric:
 
         :param l: data
         :type l: Iterable
-        :param thresh: used to determine std multiplier i.e. the observations with 
+        :param thresh: used to determine std multiplier i.e. the observations with
             probability <= 0.05 are decided as outliers.
         :type thresh: float
         :rtype: List[int]
         """
-        std_mult = norm.ppf(1-thresh/2)
+        std_mult = norm.ppf(1 - thresh / 2)
         ub = np.mean(l) + np.std(l) * std_mult
         lb = np.mean(l) - np.std(l) * std_mult
         return [idx for idx in range(len(l)) if l[idx] >= ub or l[idx] <= lb]
@@ -55,15 +57,38 @@ class OutlierMetric:
         ]
 
 
-def get_outliers(original, prediction):
+def get_outliers(
+    original: np.ndarray, prediction: np.ndarray, metric: str = "bb", **kwargs
+) -> np.ndarray:
+    """Determine idiosyncratic outliers for each reconstructed feature.
+
+    :param original: the original data
+    :type original: np.ndarray
+    :param prediction: the reconstructed data
+    :type prediction: np.ndarray
+    :param metric: the outlier metric from OutlierMetric to use for features.
+        only "bb", "quantile" and "iqr" are valid.
+    :type metric
+    :rtype: np.ndarray
+    """
     num_samples, num_features = original.shape
-    pairwise_distances = np.square(original-prediction)
+    pairwise_distances = np.square(original - prediction)
 
     m = OutlierMetric()
+    metric_dict = {
+        "bb": m.bb_outlier, 
+        "quantile": m.quantile_outlier,
+        "iqr": m.iqr_outlier
+    }
+    if metric not in metric_dict:
+        warnings.warn("Invalid metric passed. Default to bb.")
+        metric = "bb"
+    
+    outlier_func = metric_dict[metric]
 
     indices = []
     for i in range(num_features):
-        indices.append(m.bb_outlier(l=pairwise_distances[:, i]))
+        indices.append(outlier_func(pairwise_distances[:, i], **kwargs))
 
     return np.array(indices)
 
@@ -94,7 +119,7 @@ def lstm_run(
     :type epoch: int
     :param early_stopping: if to employ early stop callback
     :type early_stopping: bool
-    :rtype: (np.ndarray, Iterable)
+    :rtype: (np.ndarray, Iterable, Iterable)
     """
     metrics = OutlierMetric()
     # model training and prediction
@@ -102,9 +127,8 @@ def lstm_run(
     seq_size = model.seq_size
     model.compile(optimizer="adam", loss="mse")
     if early_stopping:
-        callback = tf.keras.callbacks.EarlyStopping(
-            monitor="loss", patience=10)
-        model.fit(
+        callback = tf.keras.callbacks.EarlyStopping(monitor="loss", patience=10)
+        history = model.fit(
             train_data,
             train_data,
             epochs=epoch,
@@ -112,7 +136,7 @@ def lstm_run(
             callbacks=[callback],
         )
     else:
-        model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size)
+        history = model.fit(train_data, train_data, epochs=epoch, batch_size=batch_size)
     model.save("lstm_model")
 
     # model prediction/reconstruction
@@ -138,7 +162,7 @@ def lstm_run(
     # else:
     #     ind = indx[metric]
 
-    return pred_reconstructed, ind
+    return pred_reconstructed, ind, history
 
 
 # def dense_run(
@@ -180,7 +204,7 @@ def temporalize(X, seq_size):
     output_X = []
 
     for i in range(len(X) - seq_size + 1):
-        output_X.append(X[i: i + seq_size, :])
+        output_X.append(X[i : i + seq_size, :])
 
     return np.array(output_X)
 
@@ -212,72 +236,79 @@ def reconstruction(seq_data, n_features):
 
 
 if __name__ == "__main__":
-    # system setup
-    print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
-    print("Num GPUs Available: ", tf.config.list_physical_devices("GPU"))
+    pass
+    # # system setup
+    # print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+    # print("Num GPUs Available: ", tf.config.list_physical_devices("GPU"))
 
-    # prepare dataset
-    featured_credit = pd.read_csv(r'data\featured_credit.csv', index_col="trans_date")
-    credit = featured_credit.loc[:, [col for col in featured_credit.columns if "_" not in col]]
-    d1 = featured_credit.loc[:, [col for col in featured_credit.columns if col.endswith("_d1")]]
+    # # prepare dataset
+    # featured_credit = pd.read_csv(r"data\featured_credit.csv", index_col="trans_date")
+    # credit = featured_credit.loc[
+    #     :, [col for col in featured_credit.columns if "_" not in col]
+    # ]
+    # d1 = featured_credit.loc[
+    #     :, [col for col in featured_credit.columns if col.endswith("_d1")]
+    # ]
 
-    # Select n companies with no zero observations and highest variances.
-    credit_nozero = credit.loc[:, credit.apply(lambda x: (x == 0).sum() == 0)]
-    # np.random.seed(42)
-    # n_companies = 6
-    np.random.seed(25)
+    # # Select n companies with no zero observations and highest variances.
+    # credit_nozero = credit.loc[:, credit.apply(lambda x: (x == 0).sum() == 0)]
+    # # np.random.seed(42)
+    # # n_companies = 6
+    # np.random.seed(25)
 
-    n_companies = 8
-    companies = np.random.choice(credit_nozero.apply(lambda x: (x - x.mean()) / x.std()).columns, n_companies,
-                                 replace=False).tolist()
-    np.random.seed(None)
+    # n_companies = 8
+    # companies = np.random.choice(
+    #     credit_nozero.apply(lambda x: (x - x.mean()) / x.std()).columns,
+    #     n_companies,
+    #     replace=False,
+    # ).tolist()
+    # np.random.seed(None)
 
-    def standard_scale(x: pd.Series):
-        return (x - x.mean()) / x.std()
-    def has_substr_in_list(s: str, l: list):
-        return not all(x not in s for x in l)
+    # def standard_scale(x: pd.Series):
+    #     return (x - x.mean()) / x.std()
 
+    # def has_substr_in_list(s: str, l: list):
+    #     return not all(x not in s for x in l)
 
-    features = featured_credit.loc[:,
-               [col for col in featured_credit if ("_" in col) and (has_substr_in_list(col, companies))]]
-    features = features.apply(standard_scale)
-    features.shape
+    # features = featured_credit.loc[
+    #     :,
+    #     [
+    #         col
+    #         for col in featured_credit
+    #         if ("_" in col) and (has_substr_in_list(col, companies))
+    #     ],
+    # ]
+    # features = features.apply(standard_scale)
+    # features.shape
 
-    # total_time = 30000
-    # seq_size = 25
-    seq_size = 5
-    n_feature = features.shape[1]
+    # # total_time = 30000
+    # # seq_size = 25
+    # seq_size = 5
+    # n_feature = features.shape[1]
 
-    data = features.values
-    test_size = 0.4
-    partition_size = int(len(data) * (1 - test_size))
+    # data = features.values
+    # test_size = 0.4
+    # partition_size = int(len(data) * (1 - test_size))
 
-    data_train = data[0:partition_size]
-    data_test = data[partition_size:]
+    # data_train = data[0:partition_size]
+    # data_test = data[partition_size:]
 
-    data_train_seq = temporalize(data_train, seq_size)
-    data_test_seq = temporalize(data_test, seq_size)
+    # data_train_seq = temporalize(data_train, seq_size)
+    # data_test_seq = temporalize(data_test, seq_size)
 
-    lstm_pred, lstm_outliers = lstm_run(
-        LSTM_Model_Base(
-            seq_size,
-            n_feature,
-            [128,
-             64,
-             64,
-             128
-             ],
-            mid_activation=tf.nn.tanh
-        ),
-        data_train_seq,
-        data_test_seq,
-        batch_size=512,
-        epoch=300,
-        metric="all",
-        early_stopping=False
-    )
-    # dense_pred, dense_outliers = dense_run(data_train, data_test, n_feature, batch_size = 100)
+    # lstm_pred, lstm_outliers = lstm_run(
+    #     LSTM_Model_Base(
+    #         seq_size, n_feature, [128, 64, 64, 128], mid_activation=tf.nn.tanh
+    #     ),
+    #     data_train_seq,
+    #     data_test_seq,
+    #     batch_size=512,
+    #     epoch=300,
+    #     metric="all",
+    #     early_stopping=False,
+    # )
+    # # dense_pred, dense_outliers = dense_run(data_train, data_test, n_feature, batch_size = 100)
 
-    print(f"Data test shape {data_test.shape}")
-    print(f"lstm_pred shape {lstm_pred.shape}")
-    # print(f"dense_pred shape {dense_pred.shape}")
+    # print(f"Data test shape {data_test.shape}")
+    # print(f"lstm_pred shape {lstm_pred.shape}")
+    # # print(f"dense_pred shape {dense_pred.shape}")
